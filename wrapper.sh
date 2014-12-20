60a26021145d458655f22a1b4949b69911f42c47
Florian Larysch <fl@n621.de>
2014-12-20 12:53:00 +0100

    wrapper: restart recorder if it exits

--- wrapper.sh
+++ wrapper.sh
@@ -3,9 +3,20 @@
 ID="$1"
 STREAM="$2"
 
-exec perl ./record.pl /tmp/hls "${STREAM}_native_sd" /srv/www/hls
+kill_children() {
+	pkill -P $$
+}
+
+trap kill_children EXIT
+
+
 if [ ! -d "/srv/www/hls/$ID" ]
 then
 	mkdir "/srv/www/hls/$ID"
 fi
 
+while true
+do
+	perl ./record.pl /tmp/hls "${STREAM}_native_sd.m3u8" /srv/www/hls/"$ID"
+	sleep 1;
+done
