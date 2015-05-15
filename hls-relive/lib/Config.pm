6675e102bfd68b16fd03dac7d1f81f5d19b301e7
Florian Larysch <fl@n621.de>
2015-05-15 13:36:51 +0200

    add config parser

---
+++ hls-relive/lib/Config.pm
@@ -0,0 +1,27 @@
+package Config;
+
+use v5.12;
+use strict;
+use warnings;
+
+sub read_config {
+	my ($path, $cb) = @_;
+
+	open(my $fh, '<', $path) or die "opening config failed: $!";
+
+	while(my $line = <$fh>) {
+		chomp($line);
+
+		next if $line =~ /^#/;
+		next if $line !~ /=/;
+
+		my ($k, $v) = split /=/, $line;
+
+		($v) = $v =~ /^"?(.*?)"?$/; #FIXME: do proper unescaping here
+
+		$cb->($k, $v);
+
+	}
+}
+
+1;
