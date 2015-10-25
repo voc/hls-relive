3c3481f6c8ba144d920c0ef79d184c3b703b6709
Florian Larysch <fl@n621.de>
2015-10-25 15:30:35 +0100

    check_released: update for new media frontend

--- hls-relive/check_released.pl
+++ hls-relive/check_released.pl
@@ -7,48 +7,60 @@ use warnings;
 use JSON;
 use Data::Dumper;
 use File::Slurp;
-use LWP;
+use LWP::Simple;
 
-sub check200 {
-	my ($url) = @_;
+sub get_json {
+        my ($url) = @_;
 
-	my $ua = LWP::UserAgent->new;
-	my $req = HTTP::Request->new(GET => $url);
+        my $doc = get($url);
+        if(not defined $doc) {
+                return undef;
+        }
 
-	my $resp = $ua->request($req);
+        my $json = decode_json($doc);
+        if(not defined $json) {
+                return undef;
+        }
 
-	return $resp->is_success;
+        return $json;
+}
+
+sub count_recordings {
+        my ($event) = @_;
+
+        my $event_data = get_json($event->{url});
+
+        if(defined($event_data) and defined($event_data->{recordings})) {
+                return scalar(@{$event_data->{recordings}});
+        } else {
+                return 0;
+        }
 }
 
 if(@ARGV != 2) {
-	say STDERR "usage: $0 media-events outfile";
-	exit 1;
+        say STDERR "usage: $0 media-events outfile";
+        exit 1;
 }
 
 my $events = decode_json(read_file($ARGV[0]))->{events};
 
 my $released_events = {};
 if(-f $ARGV[1]) {
-	$released_events = decode_json(read_file($ARGV[1]));
+        $released_events = decode_json(read_file($ARGV[1]));
 }
 
 foreach my $event (@$events) {
-	next if $released_events->{$event->{guid}};
+        next if $released_events->{$event->{guid}};
 
-	my $url = $event->{frontend_link};
-	next unless defined $url;
+        print "checking $event->{slug}... ";
 
-	# check berlin, as this is the slave mirror and thus will always be the
-	# last to receive the update
-	$url =~ s!//media.ccc.de!//berlin.media.ccc.de!g;
-
-	print "checking $url... ";
-	if(check200($url)) {
-		$released_events->{$event->{guid}} = $event->{frontend_link};
-		say "ok";
-	} else {
-		say "not ok";
-	}
+        my $recordings = count_recordings($event);
+        if($recordings > 0) {
+                $released_events->{$event->{guid}} = $event->{frontend_link};
+                say "ok ($recordings)";
+        } else {
+                say "not ok";
+        }
 }
 
 write_file($ARGV[1], encode_json($released_events));
