feb29cfb63fa6cf434b542145347dd46fc14f8c6
Florian Larysch <fl@n621.de>
2015-10-25 15:34:55 +0100

    don't check certificate when fetching schedule.xml (sorry)

--- scripts/get-fahrplan.sh
+++ scripts/get-fahrplan.sh
@@ -4,7 +4,7 @@ cd $(dirname $(realpath $0))/../
 
 source ./cfg
 
-wget -q -O data/schedule.xml.tmp "$FAHRPLAN_URL"
+wget --no-check-certificate -q -O data/schedule.xml.tmp "$FAHRPLAN_URL"
 if [ -s data/schedule.xml.tmp ]
 then
 	mv data/schedule.xml.tmp data/schedule.xml
