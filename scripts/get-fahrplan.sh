4c21acc69775475a33d1d9439ef39bcdfe61e2b4
Florian Larysch <fl@n621.de>
2015-11-17 21:10:49 +0100

    allow events without schedule.xml URL
    If no schedule URL is configured, don't try to download anything.
    
    In such a case, the schedule.xml must be placed in the appropriate
    location manually.

--- scripts/get-fahrplan.sh
+++ scripts/get-fahrplan.sh
@@ -6,6 +6,11 @@ eval $(perl "${RELIVE_REPO}/hls-relive/export-config.pl")
 DATA_DIR="${RELIVE_REPO}/data/${RELIVE_PROJECT}"
 [ -d "$DATA_DIR" ] || mkdir "$DATA_DIR"
 
+if [ -z "$FAHRPLAN_URL" ]
+then
+       exit 0
+fi
+
 wget --no-check-certificate -q -O "${DATA_DIR}/schedule.xml.tmp" "$FAHRPLAN_URL"
 if [ -s "${DATA_DIR}/schedule.xml.tmp" ]
 then
