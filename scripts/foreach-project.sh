2641d18529b6bd0996b40517cfe7d1542cf0d576
Florian Larysch <fl@n621.de>
2016-02-07 12:14:12 +0100

    allow more characters in project names

--- scripts/foreach-project.sh
+++ scripts/foreach-project.sh
@@ -3,7 +3,7 @@
 RELIVE_REPO=$(dirname $(realpath $0))/../
 CMD="$1"
 
-for project in $(find "${RELIVE_REPO}/configs/" -maxdepth 1 -type f -printf '%P\n' | egrep '^\w*$')
+for project in $(find "${RELIVE_REPO}/configs/" -maxdepth 1 -type f -printf '%P\n' | egrep '^[^.]*$')
 do
 	export RELIVE_PROJECT="$project"
 	$CMD
