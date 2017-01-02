e87ff11ea7183a20b57ff484751bc2e0b78df826
Florian Larysch <fl@n621.de>
2017-01-03 00:36:29 +0100

    foreach-project: add locking
    genpage.pl is getting fatter and fatter, which might cause it to
    occasionally take longer to run than the cron interval which starts it,
    leading to reentrancy problems.
    
    Add a per-job locking feature to foreach-project.sh, as other jobs might
    benefit from this as well.

--- scripts/foreach-project.sh
+++ scripts/foreach-project.sh
@@ -3,6 +3,30 @@
 RELIVE_REPO=$(dirname $(realpath $0))/../
 CMD="$1"
 
+lockname=/tmp/relive-$(echo "$CMD" | md5sum | cut -d' ' -f1)
+
+if [ -e "${lockname}" ]
+then
+	pid=$(cat "$lockname")
+
+	if [ -e "/proc/${pid}" ]
+	then
+		# lock is probably still alive, exit
+		exit 0
+	fi
+
+	echo "removing stale lock file ${lockname} for cmd '${CMD}'"
+	rm "${lockname}"
+fi
+
+echo "$$" > "${lockname}"
+
+cleanup() {
+	rm "${lockname}"
+}
+
+trap cleanup EXIT
+
 for project in $(find "${RELIVE_REPO}/configs/" -maxdepth 1 -type f -printf '%P\n' | egrep '^[^.]*$')
 do
 	export RELIVE_PROJECT="$project"
