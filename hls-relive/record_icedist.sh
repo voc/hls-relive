a8ca073436b734ffb87a8237ad38740e5551f86d
Florian Larysch <fl@n621.de>
2017-12-23 23:58:31 +0100

    add icedist recording mode

---
+++ hls-relive/record_icedist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STREAM="$1"
+RECORDING_DIR="$2"
+
+STARTTIME=$(date +%s)
+
+exec ffmpeg -i $STREAM -c:v copy -c:a copy \
+	-map 0:v:0 -map 0:a:0 \
+	-f hls \
+	-hls_init_time 6 -hls_time 6 \
+	-hls_list_size 0 -hls_segment_filename "${RECORDING_DIR}/${STARTTIME}-%d.ts" -hls_flags +append_list+discont_start \
+	"${RECORDING_DIR}/index.m3u8"
--- hls-relive/wrapper.sh
+++ hls-relive/wrapper.sh
@@ -19,9 +19,18 @@ then
 fi
 
 RELIVE_RESOLUTION=${RELIVE_RESOLUTION:-hd}
+RELIVE_RECORDING_MODE=${RELIVE_RECORDING_MODE:-hls}
 
 while true
 do
-	perl ./record.pl $HLS_DIR "${STREAM}_native_${RELIVE_RESOLUTION}.m3u8" $RECORDING_DIR
+	case $RELIVE_RECORDING_MODE in
+		hls)
+			perl ./record.pl $HLS_DIR "${STREAM}_native_${RELIVE_RESOLUTION}.m3u8" $RECORDING_DIR
+			;;
+		icedist)
+			./record_icedist.sh "${ICEDIST_URL}/${STREAM}" $RECORDING_DIR
+			;;
+	esac
+
 	sleep 1
 done
