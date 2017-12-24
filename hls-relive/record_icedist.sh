54051b3fe0e24d010e7048067e759ff5d837e7af
Florian Larysch <fl@n621.de>
2017-12-24 11:51:30 +0100

    record_icedist: reduce noise on stdout
    Do not print statistics as not cause too much interference between the
    output of multiple concurrent recording processes.

--- hls-relive/record_icedist.sh
+++ hls-relive/record_icedist.sh
@@ -5,7 +5,7 @@ RECORDING_DIR="$2"
 
 STARTTIME=$(date +%s)
 
-exec ffmpeg -i $STREAM -c:v copy -c:a copy \
+exec ffmpeg -nostats -i $STREAM -c:v copy -c:a copy \
 	-map 0:v:0 -map 0:a:0 \
 	-f hls \
 	-hls_time 6 -hls_list_size 0 -hls_segment_filename "${RECORDING_DIR}/${STARTTIME}-%d.ts" -hls_flags +append_list+discont_start \
