74b50efee8e8b92400f8b075a4aab34dec67c517
Andreas Hubel <andi@muc.ccc.de>
2021-04-03 17:50:11 +0200

    record second audio track + add program_date_time

--- hls-relive/record_icedist.sh
+++ hls-relive/record_icedist.sh
@@ -6,7 +6,7 @@ RECORDING_DIR="$2"
 STARTTIME=$(date +%s)
 
 exec ffmpeg -nostats -i $STREAM -c:v copy -c:a copy \
-	-map 0:v:0 -map 0:a:0 \
+	-map 0:v:0 -map 0:a:0 -map 0:a:1 \
 	-f hls \
-	-hls_time 6 -hls_list_size 0 -hls_segment_filename "${RECORDING_DIR}/${STARTTIME}-%d.ts" -hls_flags +append_list+discont_start \
+	-hls_time 6 -hls_list_size 0 -hls_segment_filename "${RECORDING_DIR}/${STARTTIME}-%d.ts" -hls_flags +append_list+discont_start+program_date_time \
 	"${RECORDING_DIR}/index.m3u8"
